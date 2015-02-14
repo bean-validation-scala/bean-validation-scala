6e5b5d6d67e6d85f28acdf8602995671b9914b1d
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2015-02-14 23:29:30 +0900

    Add a length validator.

---
+++ src/main/scala/com/tsukaby/bean_validation_scala/LengthValidatorForOption.scala
@@ -0,0 +1,29 @@
+package com.tsukaby.bean_validation_scala
+
+import javax.validation.{ConstraintValidator, ConstraintValidatorContext}
+
+import org.hibernate.validator.constraints.Length
+import org.hibernate.validator.internal.constraintvalidators._
+
+/**
+ * Check that the wrapped character sequence length is between min and max.
+ */
+class LengthValidatorForOption extends ConstraintValidator[Length, Option[_]] {
+  private var constraintAnnotation: Length = null
+
+  override def initialize(constraintAnnotation: Length): Unit = {
+    this.constraintAnnotation = constraintAnnotation
+  }
+
+  override def isValid(value: Option[_], context: ConstraintValidatorContext): Boolean = {
+
+    value match {
+      case Some(x: CharSequence) =>
+        val v = new LengthValidator
+        v.initialize(constraintAnnotation)
+        v.isValid(x, context)
+      case Some(_) | None =>
+        throw new IllegalStateException("oops.")
+    }
+  }
+}
---
+++ src/test/scala/com/tsukaby/bean_validation_scala/LengthValidatorForOptionSpec.scala
@@ -0,0 +1,24 @@
+package com.tsukaby.bean_validation_scala
+
+import org.hibernate.validator.constraints.Length
+
+import scala.annotation.meta.field
+
+class LengthValidatorForOptionSpec extends BaseSpec {
+
+  private[this] case class TestBean(
+                                     @(Length@field)(min = 1, max = 1)
+                                     value: Option[String]
+                                     )
+
+  s"$targetClassName" should {
+    Seq(
+      (TestBean(Some("")), 1),
+      (TestBean(Some("a")), 0)
+    ) foreach { case (bean, expected) =>
+      s"Check violations count. bean = $bean, count = $expected" in {
+        test(bean, expected)
+      }
+    }
+  }
+}
