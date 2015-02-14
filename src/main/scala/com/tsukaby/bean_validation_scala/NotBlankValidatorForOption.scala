37d45d55eecf497c8e18ae79b3fa264764d2faee
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2015-02-14 23:32:33 +0900

    Add a NotBlank validator.

---
+++ src/main/scala/com/tsukaby/bean_validation_scala/NotBlankValidatorForOption.scala
@@ -0,0 +1,29 @@
+package com.tsukaby.bean_validation_scala
+
+import javax.validation.{ConstraintValidator, ConstraintValidatorContext}
+
+import org.hibernate.validator.constraints.NotBlank
+import org.hibernate.validator.internal.constraintvalidators._
+
+/**
+ * Check that a wrapped character sequence's (e.g. Option[String]) trimmed length is not empty.
+ */
+class NotBlankValidatorForOption extends ConstraintValidator[NotBlank, Option[_]] {
+  private var constraintAnnotation: NotBlank = null
+
+  override def initialize(constraintAnnotation: NotBlank): Unit = {
+    this.constraintAnnotation = constraintAnnotation
+  }
+
+  override def isValid(value: Option[_], context: ConstraintValidatorContext): Boolean = {
+
+    value match {
+      case Some(x: CharSequence) =>
+        val v = new NotBlankValidator
+        v.initialize(constraintAnnotation)
+        v.isValid(x, context)
+      case Some(_) | None =>
+        throw new IllegalStateException("oops.")
+    }
+  }
+}
---
+++ src/test/scala/com/tsukaby/bean_validation_scala/NotBlankValidatorForOptionSpec.scala
@@ -0,0 +1,25 @@
+package com.tsukaby.bean_validation_scala
+
+import org.hibernate.validator.constraints.NotBlank
+
+import scala.annotation.meta.field
+
+class NotBlankValidatorForOptionSpec extends BaseSpec {
+
+  private[this] case class TestBean(
+                                     @(NotBlank@field)
+                                     value: Option[String]
+                                     )
+
+  s"$targetClassName" should {
+    Seq(
+      (TestBean(Some("")), 1),
+      (TestBean(Some(" ")), 1),
+      (TestBean(Some("a")), 0)
+    ) foreach { case (bean, expected) =>
+      s"Check violations count. bean = $bean, count = $expected" in {
+        test(bean, expected)
+      }
+    }
+  }
+}
